346df2cf6574bffde39c6ecb59c4b80014797c8a
HiteshJay <h.daryanani@eagles.oc.edu>
2024-12-11 01:22:13 +0530

    Update Board.java

--- Board.java
+++ Board.java
@@ -1,20 +1,40 @@
+import java.util.ArrayList;
+
 public class Board {
+    private ArrayList<ArrayList<CellState>> cells;
 
-	private Array<Array<CellState>> cells;
+    public Board(int size) {
+        cells = new ArrayList<>();
+        for (int i = 0; i < size; i++) {
+            ArrayList<CellState> row = new ArrayList<>();
+            for (int j = 0; j < size; j++) {
+                row.add(CellState.EMPTY);
+            }
+            cells.add(row);
+        }
+    }
 
-	/**
-	 * 
-	 * @param position
-	 * @param symbol
-	 */
-	public Boolean updateCell(Position position, Symbol symbol) {
-		// TODO - implement Board.updateCell
-		throw new UnsupportedOperationException();
-	}
+    public Boolean updateCell(Position position, Symbol symbol) {
+        if (cells.get(position.row).get(position.col) == CellState.EMPTY) {
+            cells.get(position.row).set(position.col, symbol.toCellState());
+            return true;
+        }
+        return false;
+    }
 
-	public Void resetBoard() {
-		// TODO - implement Board.resetBoard
-		throw new UnsupportedOperationException();
-	}
+    public void resetBoard() {
+        for (ArrayList<CellState> row : cells) {
+            for (int i = 0; i < row.size(); i++) {
+                row.set(i, CellState.EMPTY);
+            }
+        }
+    }
+}
 
+enum CellState {
+    EMPTY, X, O;
+
+    public static CellState fromSymbol(Symbol symbol) {
+        return symbol == Symbol.X ? X : O;
+    }
 }
