43d2fc2fd5c1845a554f2a47687da6359066ed49
HiteshJay <h.daryanani@eagles.oc.edu>
2024-12-11 01:22:31 +0530

    Update Game.java

--- Game.java
+++ Game.java
@@ -1,28 +1,44 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class Game {
+    private String gameId;
+    private List<User> players;
+    private Board board;
+    private List<Move> moves;
+    private GameStatus status;
 
-	private String gameId;
-	private List<User> players;
-	private Game board;
-	private List<Move> moves;
-	private GameStatus status;
+    public Game(String gameId, int boardSize, List<User> players) {
+        this.gameId = gameId;
+        this.board = new Board(boardSize);
+        this.players = players;
+        this.moves = new ArrayList<>();
+        this.status = GameStatus.IN_PROGRESS;
+    }
 
-	/**
-	 * 
-	 * @param player
-	 * @param position
-	 */
-	public Boolean makeMove(User player, Position position) {
-		// TODO - implement Game.makeMove
-		throw new UnsupportedOperationException();
-	}
+    public Boolean makeMove(User player, Position position) {
+        if (status != GameStatus.IN_PROGRESS) {
+            return false;
+        }
 
-	/**
-	 * 
-	 * @param winner
-	 */
-	public Void endGame(User winner) {
-		// TODO - implement Game.endGame
-		throw new UnsupportedOperationException();
-	}
+        Symbol playerSymbol = getPlayerSymbol(player);
+        if (board.updateCell(position, playerSymbol)) {
+            moves.add(new Move(this, player, position, playerSymbol));
+            return true;
+        }
+        return false;
+    }
 
+    public void endGame(User winner) {
+        this.status = GameStatus.FINISHED;
+        System.out.println("Game Over! Winner: " + (winner != null ? winner.getUsername() : "Draw"));
+    }
+
+    private Symbol getPlayerSymbol(User player) {
+        return players.get(0).equals(player) ? Symbol.X : Symbol.O;
+    }
+}
+
+enum GameStatus {
+    IN_PROGRESS, FINISHED
 }
